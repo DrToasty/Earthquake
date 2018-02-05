cb8c4ab6b371ca61a5efba792ee1b53f30780269
Peter Curry <pcurry633@gmail.com>
2018-02-05 12:09:54 +0000

    3rd version. Make the Earthquake function a callable method.

--- src/EarthquakeStrength.java
+++ src/EarthquakeStrength.java
@@ -1,33 +1,38 @@
 import java.util.Scanner; 
 public class EarthquakeStrength 
 {
-
+	public static String Earthquake(double Richter)
+	{
+		if (Richter < 4.5)
+		{
+			return "No destruction of buildings.";
+		}
+		else if (Richter >= 4.5 && Richter < 6)
+		{
+			return "Damage to poorly constructed buildings";
+		}
+		else if (Richter >= 6 && Richter < 7)
+		{
+			return "Many buildings considerably damaged, some collapse.";
+		}
+		else if (Richter >= 7 && Richter < 8)
+		{
+			return "Many buildings destroyed";
+		}
+		else  
+		{
+			return "Most structures fall";
+		}
+	}
+	
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner (System.in);//also input
 		System.out.println("Please input the value on the Richter Scale: ");
-		double Richter = in.nextDouble();
-		if (Richter < 4.5)
-		{
-			System.out.println("No destruction of buildings.");
-		}
-		else if (Richter >= 4.5 && Richter < 6)
-		{
-			System.out.println("Damage to poorly constructed buildings");
-		}
-		else if (Richter >= 6 && Richter < 7)
-		{
-			System.out.println("Many buildings considerably damaged, some collapse.");
-		}
-		else if (Richter >= 7 && Richter < 8)
-		{
-			System.out.println("Many buildings destroyed");
-		}
-		else if (Richter > 8)
-		{
-			System.out.println("Most structures fall");
-		}
+		double RichterInput = in.nextDouble();
+		System.out.println("An earthquake of " + RichterInput+" on the Richter scale would cause this much damage: \n" + Earthquake(RichterInput));
 	}
-
 }
+
+
